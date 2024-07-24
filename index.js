5720d0b88c0e31551cf43daf12a12b8b10ddc59b
habib <habibali15@hotmail.com>
2024-07-24 15:10:33 +0100

    complete epic 2 user story 1

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
-import {listOfActivities} from './activities.js'
+// import {listOfActivities} from './activities.js'
+import activityList from './activities.json' with {type:"json"};
 import express from 'express';
 import helmet from 'helmet';
 
 const app = express();
 const port = 8080;
+
 
 app.use(helmet());
 
@@ -18,10 +20,26 @@ app.listen(port, () => {
 
 // const activities = listOfActivities;
 // console.log(activities)
+// app.get('/activities', async (req, res) => {
+//   try {
+//     const activities = await listOfActivities();
+//     res.status(200).json({
+//       "success" : true,
+//       "payload" : activities
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       error: "An error occurred while fetching activities",
+//       "success" : false,
+//       "payload" : null
+//     });
+//   }
+// });
+
 
 app.get('/activities', async (req, res) => {
   try {
-    const activities = await listOfActivities();
+    const activities = await activityList;
     res.status(200).json({
       "success" : true,
       "payload" : activities
@@ -34,3 +52,21 @@ app.get('/activities', async (req, res) => {
     });
   }
 });
+
+// const activities = listOfActivities;
+// console.log(activities)
+// app.get('/activities', async (req, res) => {
+//   try {
+//     const activities = await listOfActivities();
+//     res.status(200).json({
+//       "success" : true,
+//       "payload" : activities
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       error: "An error occurred while fetching activities",
+//       "success" : false,
+//       "payload" : null
+//     });
+//   }
+// });
